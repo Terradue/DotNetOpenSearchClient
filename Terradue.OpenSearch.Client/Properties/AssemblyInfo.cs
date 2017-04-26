9dbda1c4edbd8abe9e180943bc829ab55321499c
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2017-04-26 17:37:18 +0200

    bump to 1.6.11
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

--- Terradue.OpenSearch.Client/Properties/AssemblyInfo.cs
+++ Terradue.OpenSearch.Client/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.6.10.*")]
-[assembly: AssemblyInformationalVersion("1.6.10")]
+[assembly: AssemblyVersion("1.6.11.*")]
+[assembly: AssemblyInformationalVersion("1.6.11")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
