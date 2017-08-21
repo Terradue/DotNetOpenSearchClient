277bc5621fbcc7def1776312fbb7f85743bcea31
bbrauzzi <blasco.brauzzi@terradue.com>
2017-08-21 11:46:44 +0200

    shunit usgs tests

---
+++ src/test/tests.d/test_catalog_usgs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source ./test_common.sh
+
+echo "=====  test_catalog_usgs  ====="
+
+reference="http://earthexplorer.usgs.gov"
+
+
+
+ test_id()
+{
+  local output=$( opensearch-client "${reference}" -a "${USGS_CREDENTIALS}" -m EOP -p count=1 -p pi=LANDSAT_8_C1,LANDSAT_8_PREWRS -p profile=eop -p uid=LC81920462017162LGN00  id )
+  assertEquals "LC81920462017162LGN00" "${output}"
+ }
+
+
+test_indentifier()
+{
+  local output=$( opensearch-client "${reference}" -a "${USGS_CREDENTIALS}" -m EOP -p count=1 -p pi=LANDSAT_8_C1,LANDSAT_8_PREWRS -p profile=eop -p uid=LC81920462017162LGN00  identifier )
+  assertEquals "LC81920462017162LGN00" "${output}"
+}
+
+test_enclosure()
+{
+  local output=$( opensearch-client "${reference}" -a "${USGS_CREDENTIALS}" -m EOP -p count=1 -p pi=LANDSAT_8_C1,LANDSAT_8_PREWRS -p profile=eop -p uid=LC81920462017162LGN00  enclosure )
+  assertEquals "https://earthexplorer.usgs.gov/download/external/options/LANDSAT_8_C1/LC81920462017162LGN00/INVSVC/" "${output}"
+ }
+ 
+
+
+
+. ${SHUNIT2_HOME}/shunit2
