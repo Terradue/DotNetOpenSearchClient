4ba44ee295b357467b15c2da902b85eca906e1fe
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2018-07-25 09:25:35 +0200

    DATAAUTHOR-121 + xml:lang fix in atom feed
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@terradue.com>

---
+++ Terradue.OpenSearch.Model.Base/Terradue/OpenSearch/Model/EarthObservation/CloudCoverMetadataExtractor.cs
@@ -0,0 +1,26 @@
+using System;
+using Terradue.OpenSearch.Model.GeoTime;
+using Terradue.OpenSearch.Result;
+
+namespace Terradue.OpenSearch.Model.EarthObservation
+{
+    class CloudCoverMetadataExtractor : IMetadataExtractor
+    {
+        #region IMetadataExtractor implementation
+
+        public string GetMetadata(IOpenSearchResultItem item, string specifier) {
+
+			return Terradue.Metadata.EarthObservation.OpenSearch.Extensions.EarthObservationOpenSearchResultExtensions.FindCloudCoverPercentage(item).ToString();
+        }
+
+        public string Description {
+            get {
+                return "A float number indicating the cloud coverage value in percentage of the image surface.";
+            }
+        }
+
+        #endregion
+    }
+
+}
+
