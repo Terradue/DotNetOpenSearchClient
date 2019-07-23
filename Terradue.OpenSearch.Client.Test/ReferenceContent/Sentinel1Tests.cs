526d393366332465a881d3901dcf2d2af2edb15f
floeschau <frank.loeschau@terradue.com>
2019-07-23 17:51:02 +0100

    Prepared EOP profile tests

---
+++ Terradue.OpenSearch.Client.Test/ReferenceContent/Sentinel1Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Xml;
+
+namespace Terradue.OpenSearch.Client.Test.ReferenceContent {
+
+    [TestFixture()]
+    public class NewTests : TestBase {
+
+        XmlNamespaceManager nsm;
+
+        [SetUp]
+        public void SetUpClient() {
+            LoadCredentials();
+        }
+
+        [Test()]
+        public void Test_Sentinel1_Scihub() {
+        }
+
+        [Test()]
+        public void Test_Sentinel1_OpenSearch() {
+        }
+
+
+        [Test()]
+        public void Test_Sentinel1_GeoSquare() {
+        }
+
+
+    }
+
+}
+
