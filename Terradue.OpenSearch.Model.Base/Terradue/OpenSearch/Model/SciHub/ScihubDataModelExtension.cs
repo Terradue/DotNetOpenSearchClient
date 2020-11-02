96f676bc005b0d9f4c990f81ad8c2467cd8fe328
floeschau <frank.loeschau@terradue.com>
2020-11-02 09:24:53 +0100

    Added DIAS wrappers

--- Terradue.OpenSearch.Model.Base/Terradue/OpenSearch/Model/SciHub/ScihubDataModelExtension.cs
+++ Terradue.OpenSearch.Model.Base/Terradue/OpenSearch/Model/SciHub/ScihubDataModelExtension.cs
@@ -8,6 +8,10 @@ using System.Net;
 using System.Linq;
 using Terradue.OpenSearch.DataHub;
 using Terradue.OpenSearch.Sentinel;
+using Terradue.OpenSearch.DataHub.Dias;
+using Terradue.OpenSearch.DataHub.DHuS;
+using Terradue.OpenSearch.DataHub.Aws;
+using Terradue.OpenSearch.DataHub.GoogleCloud;
 
 namespace Terradue.OpenSearch.Model.Scihub {
     
@@ -35,14 +39,86 @@ namespace Terradue.OpenSearch.Model.Scihub {
         }
 
         public override IOpenSearchable CreateOpenSearchable(IEnumerable<Uri> baseUrls, string queryFormatArg, OpenSearchEngine ose, IEnumerable<NetworkCredential> netCreds, OpenSearchableFactorySettings settings) {
-			if (netCreds == null || netCreds.Count() == 0)
-				throw new InvalidOperationException("Missing credentials for access Scihub service");
-            settings.Credentials = netCreds.First();
-            
-            LocalDataWrapper dataWrapper = new LocalDataWrapper(baseUrls.First());
-            DataHubOpenSearchable entity = new DataHubOpenSearchable(dataWrapper,settings); 
-            if (queryFormatArg == "eop") {
-                entity.DefaultMimeType = "application/atom+xml; profile=http://earth.esa.int/eop/2.1";
+
+            IDataHubSourceWrapper wrapper = null;
+            IOpenSearchable entity = null;
+
+            if (baseUrls == null || baseUrls.Count() == 0) {
+                throw new Exception("Empty list of Base URLs");
+            }
+            NetworkCredential topNetworkCredential = (netCreds == null || netCreds.Count() == 0) ? null : netCreds.First();
+
+            Uri topBaseUri = baseUrls.First();
+
+            if (topBaseUri.Host == "catalogue.onda-dias.eu") {
+                wrapper = new OndaDiasWrapper(new Uri(string.Format("https://catalogue.onda-dias.eu/dias-catalogue")), topNetworkCredential, null);
+            }
+
+            if (topBaseUri.Host == "finder.creodias.eu") {
+                wrapper = new CreoDiasWrapper(topNetworkCredential);
+            }
+
+            if (topBaseUri.Host.Contains("mundiwebservices.com")) {
+                var mundiDiasWrapper = new MundiDiasWrapper(topNetworkCredential);
+
+                if (topNetworkCredential == null) {
+                    throw new Exception("Credentials needed (<s3-key-id>:<s3-secret-key>)");
+                }
+
+                mundiDiasWrapper.S3KeyId = topNetworkCredential.UserName;
+                mundiDiasWrapper.S3SecretKey = topNetworkCredential.Password;
+                wrapper = mundiDiasWrapper;
+            }
+
+            if (topBaseUri.Host.Contains("sobloo.eu")) {
+                var soblooDiasWrapper = new SoblooDiasWrapper(topNetworkCredential);
+                wrapper = soblooDiasWrapper;
+            }
+
+            /*if (topBaseUri.Host == "api.daac.asf.alaska.edu") {
+                wrapper = new AsfApiWrapper(topBaseUri, topNetworkCredential);
+            }*/
+
+            if (topBaseUri.Host.EndsWith("copernicus.eu")) {
+                wrapper = new DHuSWrapper(topBaseUri, topNetworkCredential);
+            }
+                
+            if (topBaseUri.Host.EndsWith("amazon.com")) {
+                if (topNetworkCredential == null) {
+                    throw new Exception("Credentials needed (<s3-key-id>:<s3-secret-key>)");
+                }
+                var searchWrapper = new DHuSWrapper(new Uri("https://scihub.copernicus.eu/apihub"), topNetworkCredential);
+                // Credential is <s3-key-id>:<s3-secret-key>
+                var amazonWrapper = new AmazonWrapper(topNetworkCredential.Password, topNetworkCredential.UserName, searchWrapper);
+                wrapper = amazonWrapper;
+            }
+
+            if (topBaseUri.Host.EndsWith("googleapis.com") || topBaseUri.Host.EndsWith("google.com")) {
+                if (topNetworkCredential == null) {
+                    throw new Exception("Credentials needed (<project-id>:<google-auth-json-file>)");
+                }
+                var searchWrapper = new DHuSWrapper(new Uri("https://scihub.copernicus.eu/apihub"), topNetworkCredential);
+                // Credential is <project-id>:<google-auth-json-file>
+                var googleWrapper = new GoogleWrapper(topNetworkCredential.Password, topNetworkCredential.UserName, searchWrapper);
+                wrapper = googleWrapper;
+            }
+
+            if (wrapper == null) {
+                if (netCreds == null || netCreds.Count() == 0)
+                    throw new InvalidOperationException("Missing credentials for access Scihub service");
+                settings.Credentials = topNetworkCredential;
+
+                LocalDataWrapper dataWrapper = new LocalDataWrapper(baseUrls.First());
+                var dataHubOpenSearchable = new DataHubOpenSearchable(dataWrapper, settings);
+                if (queryFormatArg == "eop") {
+                    dataHubOpenSearchable.DefaultMimeType = "application/atom+xml; profile=http://earth.esa.int/eop/2.1";
+                }
+                entity = dataHubOpenSearchable;
+            } else {
+                OpenSearchableFactorySettings ossettings = new OpenSearchableFactorySettings(ose) {
+                    Credentials = wrapper.Settings.Credentials
+                };
+                entity = wrapper.CreateOpenSearchable(ossettings);
             }
               
             return entity;
